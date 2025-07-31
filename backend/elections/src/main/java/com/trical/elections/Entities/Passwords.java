8221c32a807e7bf2262879592210089596a819b2
Sid <siddhantd711@gmail.com>
2025-07-31 23:02:44 +0530

    all the process has done , now proceeding towards the deployment phase

--- backend/elections/src/main/java/com/trical/elections/Entities/Passwords.java
+++ backend/elections/src/main/java/com/trical/elections/Entities/Passwords.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 public class Passwords {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String email;
     private String password;
